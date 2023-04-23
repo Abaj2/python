7e357c00791a6e628dd9c8effe234a0ed423ae91
Abaj2 <130621609+Abaj2@users.noreply.github.com>
2023-04-23 22:06:43 +1000

    Update scissorspaperrock.py

--- scissorspaperrock.py
+++ scissorspaperrock.py
@@ -4,24 +4,28 @@ options = ['scissors', 'paper', 'rock']
 
 computer_choice = random.choice(options)
 
+
+
 user_choice = input('choose scissors paper or rock: ').lower()
+
+
 
 while user_choice not in options:
     user_choice = input('choose scissors paper or rock: ').lower()
 
 if user_choice == computer_choice:
     print('The computer chose ' +str(computer_choice) + ' and you chose ' +str(user_choice) +'\nThe game ended in a draw.')
-if user_choice == options[0] and computer_choice == options[1]:
+elif user_choice == options[0] and computer_choice == options[1]:
     print('The computer chose ' +str(computer_choice) + ' and you chose ' +str(user_choice) +'\nYou won!')
-if user_choice == options[0] and computer_choice == options[2]:
+elif user_choice == options[0] and computer_choice == options[2]:
     print('The computer chose ' +str(computer_choice) + ' and you chose ' +str(user_choice) + '\nThe computer won!')
-if user_choice == options[1] and computer_choice == options[0]:
+elif user_choice == options[1] and computer_choice == options[0]:
     print('The computer chose ' +str(computer_choice) +' and you chose ' +str(user_choice) +'\nThe computer Won!')
-if user_choice == options[1] and computer_choice == options[2]:
+elif user_choice == options[1] and computer_choice == options[2]:
     print('The computer chose ' +str(computer_choice) +' and you chose ' +str(user_choice) +'\nYou won!')
-if user_choice == options[2] and computer_choice == options[0]:
+elif user_choice == options[2] and computer_choice == options[0]:
     print('The computer chose ' +str(computer_choice) +' and you chose ' +str(user_choice) +'\nYou won!')
-if user_choice == options[2] and computer_choice == options[1]:
+elif user_choice == options[2] and computer_choice == options[1]:
     print('The computer chose ' +str(computer_choice) +' and you chose ' +str(user_choice) +'\nThe computer Won')
 
 
